61977e4f643f0b82754320c907eaef524733b900
Lukas Devos <ldevos98@gmail.com>
2025-01-09 12:45:12 -0500

    Rewrite `svd` to work with rectangular matrices

--- src/blocksparsearray/blockdiagonalarray.jl
+++ src/blocksparsearray/blockdiagonalarray.jl
@@ -11,38 +11,9 @@ function BlockDiagonal(blocks::AbstractVector{<:AbstractMatrix})
   )
 end
 
-# Cast to block-diagonal implementation if permuted-blockdiagonal
-function try_to_blockdiagonal_perm(A)
-  inds = map(x -> Int.(Tuple(x)), vec(collect(block_stored_indices(A))))
-  I = first.(inds)
-  allunique(I) || return nothing
-  J = last.(inds)
-  p = sortperm(J)
-  Jsorted = J[p]
-  allunique(Jsorted) || return nothing
-  return Block.(I[p], Jsorted)
-end
-
-"""
-    try_to_blockdiagonal(A)
-
-Attempt to find a permutation of blocks that makes `A` blockdiagonal. If unsuccesful,
-returns nothing, otherwise returns both the blockdiagonal `B` as well as the permutation `I, J`.
-"""
-function try_to_blockdiagonal(A::AbstractBlockSparseMatrix)
-  perm = try_to_blockdiagonal_perm(A)
-  isnothing(perm) && return perm
-  I = first.(Tuple.(perm))
-  J = last.(Tuple.(perm))
-  diagblocks = map(invperm(I), J) do i, j
-    return A[Block(i, j)]
-  end
-  return BlockDiagonal(diagblocks), perm
-end
-
 # SVD implementation
-function eigencopy_oftype(A::BlockDiagonal, S)
-  diag = map(Base.Fix2(eigencopy_oftype, S), A.blocks.diag)
+function eigencopy_oftype(A::BlockDiagonal, T)
+  diag = map(Base.Fix2(eigencopy_oftype, T), A.blocks.diag)
   return BlockDiagonal(diag)
 end
 
