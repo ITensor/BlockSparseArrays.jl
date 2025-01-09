3ecd6c8a8687787abfc45b2776d5707de15e864f
Lukas Devos <ldevos98@gmail.com>
2025-01-09 15:56:12 -0500

    Update abstractblocksparsematrix.jl
    Co-authored-by: Matt Fishman <mtfishman@users.noreply.github.com>

--- src/abstractblocksparsearray/abstractblocksparsematrix.jl
+++ src/abstractblocksparsearray/abstractblocksparsematrix.jl
@@ -49,9 +49,10 @@ function _allocate_svd_output(A::AbstractBlockSparseMatrix, full::Bool, ::Algori
     slengths[col] = min(brows[row], bcols[col])
   end
 
-  U = similar(A, axes(A, 1), blockedrange(slengths))
-  S = similar(A, real(eltype(A)), blockedrange(slengths))
-  Vt = similar(A, blockedrange(slengths), axes(A, 2))
+  s_axis = blockedrange(slengths)
+  U = similar(A, axes(A, 1), s_axis)
+  S = similar(A, real(eltype(A)), s_axis)
+  Vt = similar(A, s_axis, axes(A, 2))
 
   return U, S, Vt
 end
