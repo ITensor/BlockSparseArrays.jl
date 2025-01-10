26390049068736f82e85ba03faa7c6179dadac29
Lukas Devos <ldevos98@gmail.com>
2025-01-10 08:49:21 -0500

    remove `svd(::BlockDiagonal)` specialization

--- src/blocksparsearray/blockdiagonalarray.jl
+++ src/blocksparsearray/blockdiagonalarray.jl
@@ -4,27 +4,14 @@ using LinearAlgebra: Diagonal
 const BlockDiagonal{T,A,Axes,V<:AbstractVector{A}} = BlockSparseMatrix{
   T,A,Diagonal{A,V},Axes
 }
+const BlockSparseDiagonal{T,A<:AbstractBlockSparseVector{T}} = Diagonal{T,A}
+
+@interface interface::BlockSparseArrayInterface function blocks(a::BlockSparseDiagonal)
+  return Diagonal(Diagonal.(blocks(a.diag)))
+end
 
 function BlockDiagonal(blocks::AbstractVector{<:AbstractMatrix})
   return BlockSparseArray(
     Diagonal(blocks), (blockedrange(size.(blocks, 1)), blockedrange(size.(blocks, 2)))
   )
-end
-
-# SVD implementation
-function eigencopy_oftype(A::BlockDiagonal, T)
-  diag = map(Base.Fix2(eigencopy_oftype, T), LinearAlgebra.diag(blocks(A)))
-  return BlockDiagonal(diag)
-end
-
-function svd(A::BlockDiagonal; kwargs...)
-  return svd!(eigencopy_oftype(A, LinearAlgebra.eigtype(eltype(A))); kwargs...)
-end
-function svd!(A::BlockDiagonal; full::Bool=false, alg::Algorithm=default_svd_alg(A))
-  # TODO: handle full
-  F = map(a -> svd!(a; full, alg), blocks(A).diag)
-  Us = map(Base.Fix2(getproperty, :U), F)
-  Ss = map(Base.Fix2(getproperty, :S), F)
-  Vts = map(Base.Fix2(getproperty, :Vt), F)
-  return SVD(BlockDiagonal(Us), mortar(Ss), BlockDiagonal(Vts))
 end
