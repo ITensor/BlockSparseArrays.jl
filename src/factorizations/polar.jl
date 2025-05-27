969b49ec475545740f1c6c9faa37345832a556fa
mtfishman <mfishman@flatironinstitute.org>
2025-05-27 13:52:56 -0400

    Improve polar a bit

--- src/factorizations/polar.jl
+++ src/factorizations/polar.jl
@@ -7,31 +7,35 @@ using MatrixAlgebraKit:
   right_polar!,
   svd_compact!
 
-function MatrixAlgebraKit.check_input(
-  ::typeof(left_polar!), A::AbstractBlockSparseMatrix, WP
-)
-  W, P = WP
+function MatrixAlgebraKit.check_input(::typeof(left_polar!), A::AbstractBlockSparseMatrix)
   @views for I in eachblockstoredindex(A)
     m, n = size(A[I])
     m >= n ||
       throw(ArgumentError("each input matrix block needs at least as many rows as columns"))
-    # check_input(left_polar!, A[I], (W[I1], P[I2]))
+  end
+  return nothing
+end
+function MatrixAlgebraKit.check_input(::typeof(right_polar!), A::AbstractBlockSparseMatrix)
+  @views for I in eachblockstoredindex(A)
+    m, n = size(A[I])
+    m <= n ||
+      throw(ArgumentError("each input matrix block needs at least as many columns as rows"))
   end
   return nothing
 end
 
-function MatrixAlgebraKit.left_polar!(A::AbstractBlockSparseMatrix, WP, alg::PolarViaSVD)
-  check_input(left_polar!, A, WP)
-  U, S, Vᴴ = svd_compact!(A, alg.svdalg)
+function MatrixAlgebraKit.left_polar!(A::AbstractBlockSparseMatrix, alg::PolarViaSVD)
+  check_input(left_polar!, A)
   # TODO: Use more in-place operations here, avoid `copy`.
+  U, S, Vᴴ = svd_compact!(A, alg.svdalg)
   W = U * Vᴴ
   P = copy(Vᴴ') * S * Vᴴ
   return (W, P)
 end
-function MatrixAlgebraKit.right_polar!(A::AbstractBlockSparseMatrix, PWᴴ, alg::PolarViaSVD)
-  check_input(right_polar!, A, PWᴴ)
-  U, S, Vᴴ = svd_compact!(A, alg.svdalg)
+function MatrixAlgebraKit.right_polar!(A::AbstractBlockSparseMatrix, alg::PolarViaSVD)
+  check_input(right_polar!, A)
   # TODO: Use more in-place operations here, avoid `copy`.
+  U, S, Vᴴ = svd_compact!(A, alg.svdalg)
   Wᴴ = U * Vᴴ
   P = U * S * copy(U')
   return (P, Wᴴ)
