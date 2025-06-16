de3e859c257bbbdf27d1f37faf02e488d9335a49
mtfishman <mfishman@flatironinstitute.org>
2025-06-16 13:25:11 -0400

    Make block sparse SVD more generic

--- src/blocksparsearrayinterface/getunstoredblock.jl
+++ src/blocksparsearrayinterface/getunstoredblock.jl
@@ -5,6 +5,35 @@ struct GetUnstoredBlock{Axes}
   axes::Axes
 end
 
+# Allow customizing based on the block index.
+function unstored_block(
+  A::Type{<:AbstractArray{<:Any,N}}, ax::NTuple{N,AbstractUnitRange{<:Integer}}, I::Block{N}
+) where {N}
+  return unstored_block(A, ax)
+end
+function unstored_block(
+  A::Type{<:AbstractArray{<:Any,N}}, ax::NTuple{N,AbstractUnitRange{<:Integer}}
+) where {N}
+  a = similar(A, ax)
+  zero!(a)
+  return a
+end
+
+using LinearAlgebra: Diagonal
+# TODO: This is a hack and is also type-unstable.
+function unstored_block(
+  A::Type{<:Diagonal{<:Any,V}}, ax::NTuple{2,AbstractUnitRange{<:Integer}}, I::Block{2}
+) where {V}
+  if allequal(Tuple(I))
+    # Diagonal blocks.
+    diag = zero!(similar(V, first(ax)))
+    return Diagonal(diag)
+  else
+    # Off-diagonal blocks.
+    return zero!(similar(similartype(V, typeof(ax)), ax))
+  end
+end
+
 @inline function (f::GetUnstoredBlock)(
   a::AbstractArray{<:Any,N}, I::Vararg{Int,N}
 ) where {N}
@@ -13,28 +42,11 @@ end
   b_ax = ntuple(ndims(a)) do d
     return only(axes(f.axes[d][Block(I[d])]))
   end
-  b = similar(eltype(a), b_ax)
-  zero!(b)
-  return b
+  return unstored_block(eltype(a), b_ax, Block(I))
 end
 # TODO: Use `Base.to_indices`.
 @inline function (f::GetUnstoredBlock)(
   a::AbstractArray{<:Any,N}, I::CartesianIndex{N}
 ) where {N}
   return f(a, Tuple(I)...)
-end
-
-# TODO: this is a hack and is also type-unstable
-function (f::GetUnstoredBlock)(
-  a::AbstractMatrix{LinearAlgebra.Diagonal{T,V}}, I::Vararg{Int,2}
-) where {T,V}
-  b_size = ntuple(ndims(a)) do d
-    return length(f.axes[d][Block(I[d])])
-  end
-  if I[1] == I[2]
-    diag = zero!(similar(V, b_size[1]))
-    return LinearAlgebra.Diagonal{T,V}(diag)
-  else
-    return zeros(T, b_size...)
-  end
 end
