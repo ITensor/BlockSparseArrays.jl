d926f52a1ed8d5b276e2379147589ef15ef3d757
Lukas Devos <ldevos98@gmail.com>
2025-08-15 12:39:31 +0200

    Refactor LQ

--- src/factorizations/lq.jl
+++ src/factorizations/lq.jl
@@ -8,211 +8,140 @@ function MatrixAlgebraKit.default_lq_algorithm(
   end
 end
 
-function similar_output(
-  ::typeof(lq_compact!), A, L_axis, alg::MatrixAlgebraKit.AbstractAlgorithm
-)
-  L = similar(A, axes(A, 1), L_axis)
-  Q = similar(A, L_axis, axes(A, 2))
-  return L, Q
-end
-
-function similar_output(
-  ::typeof(lq_full!), A, L_axis, alg::MatrixAlgebraKit.AbstractAlgorithm
-)
-  L = similar(A, axes(A, 1), L_axis)
-  Q = similar(A, L_axis, axes(A, 2))
-  return L, Q
+function output_type(
+  f::Union{typeof(lq_compact!),typeof(lq_full!)}, A::Type{<:AbstractMatrix{T}}
+) where {T}
+  LQ = Base.promote_op(f, A)
+  return isconcretetype(LQ) ? LQ : Tuple{AbstractMatrix{T},AbstractMatrix{T}}
 end
 
 function MatrixAlgebraKit.initialize_output(
-  ::typeof(lq_compact!), A::AbstractBlockSparseMatrix, alg::BlockPermutedDiagonalAlgorithm
+  ::typeof(lq_compact!), ::AbstractBlockSparseMatrix, ::BlockPermutedDiagonalAlgorithm
 )
-  bm, bn = blocksize(A)
-  bmn = min(bm, bn)
-
+  return nothing
+end
+function MatrixAlgebraKit.initialize_output(
+  ::typeof(lq_compact!), A::AbstractBlockSparseMatrix, alg::BlockDiagonalAlgorithm
+)
   brows = eachblockaxis(axes(A, 1))
   bcols = eachblockaxis(axes(A, 2))
-  l_axes = similar(brows, bmn)
-
-  # fill in values for blocks that are present
-  bIs = collect(eachblockstoredindex(A))
-  browIs = Int.(first.(Tuple.(bIs)))
-  bcolIs = Int.(last.(Tuple.(bIs)))
-  for bI in eachblockstoredindex(A)
-    row, col = Int.(Tuple(bI))
-    len = minimum(length, (brows[row], bcols[col]))
-    l_axes[row] = bcols[col][Base.OneTo(len)]
-  end
-
-  # fill in values for blocks that aren't present, pairing them in order of occurence
-  # this is a convention, which at least gives the expected results for blockdiagonal
-  emptyrows = setdiff(1:bm, browIs)
-  emptycols = setdiff(1:bn, bcolIs)
-  for (row, col) in zip(emptyrows, emptycols)
-    len = minimum(length, (brows[row], bcols[col]))
-    l_axes[row] = bcols[col][Base.OneTo(len)]
-  end
-
+  # using the property that zip stops as soon as one of the iterators is exhausted
+  l_axes = map(splat(infimum), zip(brows, bcols))
   l_axis = mortar_axis(l_axes)
-  L, Q = similar_output(lq_compact!, A, l_axis, alg)
 
-  # allocate output
-  for bI in eachblockstoredindex(A)
-    brow, bcol = Tuple(bI)
-    block = @view!(A[bI])
-    block_alg = block_algorithm(alg, block)
-    L[brow, brow], Q[brow, bcol] = MatrixAlgebraKit.initialize_output(
-      lq_compact!, block, block_alg
-    )
-  end
-
-  # allocate output for blocks that aren't present -- do we also fill identities here?
-  for (row, col) in zip(emptyrows, emptycols)
-    @view!(Q[Block(row, col)])
-  end
+  BL, BQ = fieldtypes(output_type(lq_compact!, blocktype(A)))
+  L = similar(A, BlockType(BL), (axes(A, 1), l_axis))
+  Q = similar(A, BlockType(BQ), (l_axis, axes(A, 2)))
 
   return L, Q
 end
 
 function MatrixAlgebraKit.initialize_output(
-  ::typeof(lq_full!), A::AbstractBlockSparseMatrix, alg::BlockPermutedDiagonalAlgorithm
+  ::typeof(lq_full!), ::AbstractBlockSparseMatrix, ::BlockPermutedDiagonalAlgorithm
 )
-  bm, bn = blocksize(A)
-
-  bcols = eachblockaxis(axes(A, 2))
-  l_axes = copy(bcols)
-
-  # fill in values for blocks that are present
-  bIs = collect(eachblockstoredindex(A))
-  browIs = Int.(first.(Tuple.(bIs)))
-  bcolIs = Int.(last.(Tuple.(bIs)))
-  for bI in eachblockstoredindex(A)
-    row, col = Int.(Tuple(bI))
-    l_axes[row] = bcols[col]
-  end
-
-  # fill in values for blocks that aren't present, pairing them in order of occurence
-  # this is a convention, which at least gives the expected results for blockdiagonal
-  emptyrows = setdiff(1:bm, browIs)
-  emptycols = setdiff(1:bn, bcolIs)
-  for (row, col) in zip(emptyrows, emptycols)
-    l_axes[row] = bcols[col]
-  end
-  for (i, k) in enumerate((length(emptycols) + 1):length(emptyrows))
-    l_axes[bn + i] = bcols[emptycols[k]]
-  end
-
-  l_axis = mortar_axis(l_axes)
-  L, Q = similar_output(lq_full!, A, l_axis, alg)
-
-  # allocate output
-  for bI in eachblockstoredindex(A)
-    brow, bcol = Tuple(bI)
-    block = @view!(A[bI])
-    block_alg = block_algorithm(alg, block)
-    L[brow, brow], Q[brow, bcol] = MatrixAlgebraKit.initialize_output(
-      lq_full!, block, block_alg
-    )
-  end
-
-  # allocate output for blocks that aren't present -- do we also fill identities here?
-  for (row, col) in zip(emptyrows, emptycols)
-    @view!(Q[Block(row, col)])
-  end
-  # also handle extra rows/cols
-  for (i, k) in enumerate((length(emptyrows) + 1):length(emptycols))
-    @view!(Q[Block(bm + i, emptycols[k])])
-  end
-
+  return nothing
+end
+function MatrixAlgebraKit.initialize_output(
+  ::typeof(lq_full!), A::AbstractBlockSparseMatrix, alg::BlockDiagonalAlgorithm
+)
+  BL, BQ = fieldtypes(output_type(lq_full!, blocktype(A)))
+  L = similar(A, BlockType(BL), (axes(A, 1), axes(A, 2)))
+  Q = similar(A, BlockType(BQ), (axes(A, 2), axes(A, 2)))
   return L, Q
 end
 
 function MatrixAlgebraKit.check_input(
-  ::typeof(lq_compact!), A::AbstractBlockSparseMatrix, LQ
+  ::typeof(lq_compact!), A::AbstractBlockSparseMatrix, LQ, ::BlockPermutedDiagonalAlgorithm
 )
-  L, Q = LQ
-  @assert isa(L, AbstractBlockSparseMatrix) && isa(Q, AbstractBlockSparseMatrix)
-  @assert eltype(A) == eltype(L) == eltype(Q)
-  @assert axes(A, 1) == axes(L, 1) && axes(A, 2) == axes(Q, 2)
-  @assert axes(L, 2) == axes(Q, 1)
-
+  @assert isblockpermuteddiagonal(A)
   return nothing
 end
-
-function MatrixAlgebraKit.check_input(::typeof(lq_full!), A::AbstractBlockSparseMatrix, LQ)
-  L, Q = LQ
+function MatrixAlgebraKit.check_input(
+  ::typeof(lq_compact!), A::AbstractBlockSparseMatrix, (L, Q), ::BlockDiagonalAlgorithm
+)
   @assert isa(L, AbstractBlockSparseMatrix) && isa(Q, AbstractBlockSparseMatrix)
   @assert eltype(A) == eltype(L) == eltype(Q)
   @assert axes(A, 1) == axes(L, 1) && axes(A, 2) == axes(Q, 2)
   @assert axes(L, 2) == axes(Q, 1)
-
+  @assert isblockdiagonal(A)
+  return nothing
+end
+function MatrixAlgebraKit.check_input(
+  ::typeof(lq_full!), A::AbstractBlockSparseMatrix, LQ, ::BlockPermutedDiagonalAlgorithm
+)
+  @assert isblockpermuteddiagonal(A)
+  return nothing
+end
+function MatrixAlgebraKit.check_input(
+  ::typeof(lq_full!), A::AbstractBlockSparseMatrix, (L, Q), ::BlockDiagonalAlgorithm
+)
+  @assert isa(L, AbstractBlockSparseMatrix) && isa(Q, AbstractBlockSparseMatrix)
+  @assert eltype(A) == eltype(L) == eltype(Q)
+  @assert axes(A, 1) == axes(L, 1) && axes(A, 2) == axes(Q, 2)
+  @assert axes(L, 2) == axes(Q, 1)
+  @assert isblockdiagonal(A)
   return nothing
 end
 
 function MatrixAlgebraKit.lq_compact!(
   A::AbstractBlockSparseMatrix, LQ, alg::BlockPermutedDiagonalAlgorithm
 )
-  MatrixAlgebraKit.check_input(lq_compact!, A, LQ)
-  L, Q = LQ
+  MatrixAlgebraKit.check_input(lq_compact!, A, LQ, alg)
+  Ad, transform_rows, transform_cols = blockdiagonalize(A)
+  Ld, Qd = lq_compact!(Ad, BlockDiagonalAlgorithm(alg))
+  L = transform_rows(Ld)
+  Q = transform_cols(Qd)
+  return L, Q
+end
+function MatrixAlgebraKit.lq_compact!(
+  A::AbstractBlockSparseMatrix, (L, Q), alg::BlockDiagonalAlgorithm
+)
+  MatrixAlgebraKit.check_input(lq_compact!, A, (L, Q), alg)
 
   # do decomposition on each block
-  for bI in eachblockstoredindex(A)
-    brow, bcol = Tuple(bI)
-    lq = (@view!(L[brow, brow]), @view!(Q[brow, bcol]))
-    block = @view!(A[bI])
-    block_alg = block_algorithm(alg, block)
-    lq′ = lq_compact!(block, lq, block_alg)
-    @assert lq === lq′ "lq_compact! might not be in-place"
+  for I in 1:min(blocksize(A)...)
+    bI = Block(I, I)
+    if isstored(blocks(A), CartesianIndex(I, I)) # TODO: isblockstored
+      block = @view!(A[bI])
+      block_alg = block_algorithm(alg, block)
+      bL, bQ = lq_compact!(block, block_alg)
+      L[bI] = bL
+      Q[bI] = bQ
+    else
+      copyto!(@view!(Q[bI]), LinearAlgebra.I)
+    end
   end
 
-  # fill in identities for blocks that aren't present
-  bIs = collect(eachblockstoredindex(A))
-  browIs = Int.(first.(Tuple.(bIs)))
-  bcolIs = Int.(last.(Tuple.(bIs)))
-  emptyrows = setdiff(1:blocksize(A, 1), browIs)
-  emptycols = setdiff(1:blocksize(A, 2), bcolIs)
-  # needs copyto! instead because size(::LinearAlgebra.I) doesn't work
-  # Q[Block(row, col)] = LinearAlgebra.I
-  for (row, col) in zip(emptyrows, emptycols)
-    copyto!(@view!(Q[Block(row, col)]), LinearAlgebra.I)
-  end
-
-  return LQ
+  return L, Q
 end
 
 function MatrixAlgebraKit.lq_full!(
   A::AbstractBlockSparseMatrix, LQ, alg::BlockPermutedDiagonalAlgorithm
 )
-  MatrixAlgebraKit.check_input(lq_full!, A, LQ)
-  L, Q = LQ
+  MatrixAlgebraKit.check_input(lq_full!, A, LQ, alg)
+  Ad, transform_rows, transform_cols = blockdiagonalize(A)
+  Ld, Qd = lq_full!(Ad, BlockDiagonalAlgorithm(alg))
+  L = transform_rows(Ld)
+  Q = transform_cols(Qd)
+  return L, Q
+end
+function MatrixAlgebraKit.lq_full!(
+  A::AbstractBlockSparseMatrix, (L, Q), alg::BlockDiagonalAlgorithm
+)
+  MatrixAlgebraKit.check_input(lq_full!, A, (L, Q), alg)
 
-  # do decomposition on each block
-  for bI in eachblockstoredindex(A)
-    brow, bcol = Tuple(bI)
-    lq = (@view!(L[brow, brow]), @view!(Q[brow, bcol]))
-    block = @view!(A[bI])
-    block_alg = block_algorithm(alg, block)
-    lq′ = lq_full!(block, lq, block_alg)
-    @assert lq === lq′ "lq_full! might not be in-place"
+  for I in 1:min(blocksize(A)...)
+    bI = Block(I, I)
+    if isstored(blocks(A), CartesianIndex(I, I)) # TODO: isblockstored
+      block = @view!(A[bI])
+      block_alg = block_algorithm(alg, block)
+      bL, bQ = lq_full!(block, block_alg)
+      L[bI] = bL
+      Q[bI] = bQ
+    else
+      copyto!(@view!(Q[bI]), LinearAlgebra.I)
+    end
   end
 
-  # fill in identities for blocks that aren't present
-  bIs = collect(eachblockstoredindex(A))
-  browIs = Int.(first.(Tuple.(bIs)))
-  bcolIs = Int.(last.(Tuple.(bIs)))
-  emptyrows = setdiff(1:blocksize(A, 1), browIs)
-  emptycols = setdiff(1:blocksize(A, 2), bcolIs)
-  # needs copyto! instead because size(::LinearAlgebra.I) doesn't work
-  # Q[Block(row, col)] = LinearAlgebra.I
-  for (row, col) in zip(emptyrows, emptycols)
-    copyto!(@view!(Q[Block(row, col)]), LinearAlgebra.I)
-  end
-
-  # also handle extra rows/cols
-  bm = blocksize(A, 1)
-  for (i, k) in enumerate((length(emptyrows) + 1):length(emptycols))
-    copyto!(@view!(Q[Block(bm + i, emptycols[k])]), LinearAlgebra.I)
-  end
-
-  return LQ
+  return L, Q
 end
