484e347ea3f842369a3cefa0cfd2a11f59a7dab9
mtfishman <mfishman@flatironinstitute.org>
2025-08-16 20:10:21 -0400

    More general fix

--- src/BlockArraysExtensions/BlockArraysExtensions.jl
+++ src/BlockArraysExtensions/BlockArraysExtensions.jl
@@ -277,12 +277,11 @@ function _blockreshape(a::AbstractArray, axes::Tuple{Vararg{AbstractUnitRange}})
     # TODO: Better converter here.
     return reshape(reshaped_blocks_a[I], block_axes_I)
   end
-  bs = if iszero(storedlength(reshaped_blocks_a))
+  bs = Dict(Block(Tuple(I)) => f(I) for I in eachstoredindex(reshaped_blocks_a))
+  if !isconcretetype(eltype(bs))
     # This branch only seems to be required in Julia 1.10, not Julia 1.11.
     # TODO: Remove this branch once Julia 1.10 support is dropped.
-    Dict{Block{length(axes),Int},AbstractArray{eltype(a),length(axes)}}()
-  else
-    Dict(Block(Tuple(I)) => f(I) for I in eachstoredindex(reshaped_blocks_a))
+    bs = Dict{Block{length(axes),Int},AbstractArray{eltype(a),length(axes)}}()
   end
   return blocksparse(bs, axes)
 end
